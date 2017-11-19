0e4258c0ff7d45c16060c2cce8e74fe0d12cb357
eden90267 <eden90267@gmail.com>
2017-11-20 00:09:59 +0800

    Learning ch07

---
+++ ch07/lightbox-nivo/js/all.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+  $(window).load(function() {
+    $('.jq-nivo').nivoSlider();  
+  }); 
+  // lightbox 效果
+  // lightbox.option({
+  //       'resizeDuration': 2000,
+  //       'wrapAround': true
+  //     });
+ 
+});
